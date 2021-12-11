ca056280fd16a18fd2214f2fe884894f2b068f98
lvjiachen <lvjiachen@chinayasha.com>
2021-12-11 21:02:45 +0800

    1

--- dream-web/src/main/java/com/dream/web/application/Controller/HelloWorldController.java
+++ dream-web/src/main/java/com/dream/web/application/Controller/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.dream.web.application.Controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 @RequestMapping("HelloWorld")
 public class HelloWorldController {
