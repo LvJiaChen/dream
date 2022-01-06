3ba2a4ae559cde3ae050bfbd70bc3dc720dbe037
lvjiachen <lvjiachen@chinayasha.com>
2022-01-06 20:55:17 +0800

    1

--- vue-manage-system-master/src/request/login.js
+++
@@ -1,8 +0,0 @@
-import request from '../utils/request'
-export function loginSubmit(data){
-    return request({
-        url:"/wms-user/login",
-        method:"post",
-        params:data
-    });
-}
