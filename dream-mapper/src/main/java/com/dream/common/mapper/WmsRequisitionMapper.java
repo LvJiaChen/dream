3e4f83519ad8ba7bfc01f2d442c505d8df18356e
lvjiachen <lvjiachen@chinayasha.com>
2022-02-18 16:35:35 +0800

    1

--- dream-mapper/src/main/java/com/dream/common/mapper/WmsRequisitionMapper.java
+++ dream-mapper/src/main/java/com/dream/common/mapper/WmsRequisitionMapper.java
@@ -1,7 +1,14 @@
 package com.dream.common.mapper;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dream.common.entity.WmsRequisition;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dream.common.vo.WmsRequisitionDetailVo;
+import com.dream.common.vo.WmsRequisitionPageVo;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -13,4 +20,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface WmsRequisitionMapper extends BaseMapper<WmsRequisition> {
 
+    IPage<WmsRequisitionPageVo> queryRequisitionListPage(Page<WmsRequisitionPageVo> entryPageVoPage, String code, String materialName);
+
+    List<WmsRequisitionDetailVo> queryRequisitionDetail(Map param);
 }
